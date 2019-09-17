0ffff7347dfa960c37c647c4f331e30394ba94d4
Janson <jansonchiu343@gmail.com>
2019-09-17 01:33:19 -0700

    add onSubmit function and addTodo EventEmitter

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
+  /**  */
+  @Output() addTodo: EventEmitter<any> = new EventEmitter();
+
   /** Title of the Todo*/
   title: string;
   constructor() { }
@@ -14,4 +17,12 @@ export class AddTodoComponent implements OnInit {
   ngOnInit() {
   }
 
+  /** Submit button clicked */
+  onSubmit() {
+    const todo = { 
+      title: this.title,
+      completed: false
+    }
+    this.addTodo.emit(todo);
+  }
 }
